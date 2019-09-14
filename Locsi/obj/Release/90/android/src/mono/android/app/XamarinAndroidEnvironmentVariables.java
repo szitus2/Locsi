62ca2cbd845643c6e1abf33848cc211532d5aa17
akos.szitanyi <akos.szitanyi@PAPA>
2019-09-15 01:52:38 +0200

    1.2 Playstore

--- Locsi/obj/Release/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ Locsi/obj/Release/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_DEBUG", "gen-compact-seq-points",
-		"XAMARIN_BUILD_ID", "c6b46f0a-3ddd-4319-8695-504e3919188d",
+		"XAMARIN_BUILD_ID", "13ad7fb5-9671-4948-890d-b7ad34210109",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
