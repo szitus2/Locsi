38b1838a0917acd1a6b30e5e1b487e7a123c6b90
akos.szitanyi <akos.szitanyi@PAPA>
2019-09-14 16:40:44 +0200

    1.2RC2

--- Locsi/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ Locsi/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "9dffb651-31fe-4aab-9d1d-adcf5b04a86f",
+		"XAMARIN_BUILD_ID", "ffb0f2c3-3a44-4f13-888d-b48e9031a11e",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
--- Locsi/obj/Release/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ Locsi/obj/Release/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_DEBUG", "gen-compact-seq-points",
-		"XAMARIN_BUILD_ID", "0679fa72-d7c1-4a43-99b6-5d4c71c7022d",
+		"XAMARIN_BUILD_ID", "c6b46f0a-3ddd-4319-8695-504e3919188d",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
